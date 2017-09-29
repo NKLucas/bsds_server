46e45a32132bec0541662226c435d2086f0203d0
nklucas <lizhiyu1991@gmail.com>
2017-09-29 00:36:41 -0700

    step 3, change the baseurl to remote.

--- src/main/java/edu/neu/zhiyu/client/WSClientMain.java
+++ src/main/java/edu/neu/zhiyu/client/WSClientMain.java
@@ -2,9 +2,13 @@ package edu.neu.zhiyu.client;
 
 
 public class WSClientMain {
+
+    private static String baseUrl = "http://ec2-34-209-86-171.us-west-2.compute.amazonaws.com:8080/bsds-server/api/";
+
     public static void main(String[] argv) {
         // Please, do not remove this line from file template, here invocation of web service will be inserted
-        WSClient client = new WSClient("http://localhost:9234/api/status");
+        String url = baseUrl + "status";
+        WSClient client = new WSClient(url);
         String status = client.getStatus();
         System.out.println("getStatus got response: " + status);
 
