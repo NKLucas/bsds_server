5af01c8ebe65d7c0c40bfaea8bf2e8d4dd76f736
nklucas <lizhiyu1991@gmail.com>
2017-09-29 15:41:37 -0700

    step 4 finished: add the count and take arguments

--- src/main/java/edu/neu/zhiyu/client/WSClient.java
+++ src/main/java/edu/neu/zhiyu/client/WSClient.java
@@ -4,24 +4,52 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class WSClient {
+public class WSClient extends Thread {
 
     private WebTarget webTarget;
+    private int iterations;
+    private AtomicInteger totalRequest;
+    private AtomicInteger successRequest;
+    private String testMessage = "OK! This is a test!";
 
-    public WSClient(String url) {
+    public WSClient(String url, int iterations, AtomicInteger totalRequest, AtomicInteger successRequest) {
         Client client = ClientBuilder.newClient();
         this.webTarget = client.target(url);
+        this.iterations = iterations;
+        this.totalRequest = totalRequest;
+        this.successRequest = successRequest;
+
     }
 
     public int postText(Object requestEntity) throws ClientErrorException {
-        return webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(requestEntity,
+        totalRequest.getAndAdd(1);
+        int length = webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(requestEntity,
                 javax.ws.rs.core.MediaType.TEXT_PLAIN),
                 Integer.class);
+
+        if (length == testMessage.length()) {
+            successRequest.getAndAdd(1);
+        }
+        return length;
     }
 
     public String getStatus() throws ClientErrorException {
-        WebTarget resource = webTarget;
-        return resource.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+        totalRequest.getAndAdd(1);
+        String status = webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+        if (status.equals("alive")){
+            successRequest.getAndAdd(1);
+        }
+        return status;
     }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < iterations; i++) {
+            this.getStatus();
+            this.postText(testMessage);
+        }
+    }
+
 }
--- src/main/java/edu/neu/zhiyu/client/WSClientMain.java
+++ src/main/java/edu/neu/zhiyu/client/WSClientMain.java
@@ -1,19 +1,64 @@
 package edu.neu.zhiyu.client;
 
 
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class WSClientMain {
 
-    private static String baseUrl = "http://ec2-34-209-86-171.us-west-2.compute.amazonaws.com:8080/bsds-server/api/";
+    private static String baseUrl = "http://ec2-34-209-86-171.us-west-2.compute.amazonaws.com";
+    private static String defaultPort = "8080";
+    private static String serviceName = "bsds-server";
+    private static String apiPath = "/api/status";
+    private static String localUrl = "http://localhost:9234/api/status";
 
     public static void main(String[] argv) {
-        // Please, do not remove this line from file template, here invocation of web service will be inserted
-        String url = baseUrl + "status";
-        WSClient client = new WSClient(url);
-        String status = client.getStatus();
-        System.out.println("getStatus got response: " + status);
+        String base = baseUrl;
+        String port = defaultPort;
+        int MAX_THREAD = 10;
+        int ITERATIONS = 100;
+        AtomicInteger totalRequest = new AtomicInteger(0);
+        AtomicInteger successRequest = new AtomicInteger(0);
 
-        String message = "OK! This is a test!";
-        int count = client.postText(message);
-        System.out.println("postText got response: " + count);
+        if (argv.length == 4) {
+            MAX_THREAD = Integer.valueOf(argv[0]);
+            ITERATIONS = Integer.valueOf(argv[1]);
+            base = argv[2];
+            port = argv[3];
+        }
+
+        System.out.println("Server Address: " + base);
+        System.out.println("Connect Port: " + port);
+        System.out.println("API Path: " + apiPath);
+        System.out.println("Max Threads: " + MAX_THREAD);
+        System.out.println("Iterations: " + ITERATIONS + "\n");
+
+        System.out.println("Client stating...");
+        System.out.println("Current time: " + new Date().toString() + "\n");
+
+        String url = base + ":" + port + "/" + serviceName + apiPath;
+        ExecutorService ex = Executors.newFixedThreadPool(MAX_THREAD);
+
+        long start = System.currentTimeMillis();
+
+        for (int id = 0; id < MAX_THREAD; id++) {
+            WSClient client = new WSClient(url, ITERATIONS, totalRequest, successRequest);
+            ex.submit(client);
+        }
+
+        System.out.println("All threads are now running...");
+
+        ex.shutdown();
+        while (!ex.isTerminated()) {}
+        long end = System.currentTimeMillis();
+
+        System.out.println("All threads are now completed.");
+        System.out.println("Total request send: " + totalRequest);
+        System.out.println("Total successful responses: " + successRequest);
+        System.out.println();
+        System.out.println("Current time: " + new Date().toString());
+        System.out.println("Total time taken: " + (end - start) + "ms");
     }
 }
