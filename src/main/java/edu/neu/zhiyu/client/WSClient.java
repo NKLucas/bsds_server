83ba3bcb056d09f08e096fbb471363afc3aea52c
nklucas <lizhiyu1991@gmail.com>
2017-09-29 17:00:22 -0700

    Step 5 finished: Add metrics for the call latencies.

--- src/main/java/edu/neu/zhiyu/client/WSClient.java
+++ src/main/java/edu/neu/zhiyu/client/WSClient.java
@@ -4,6 +4,8 @@ import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class WSClient extends Thread {
@@ -13,6 +15,8 @@ public class WSClient extends Thread {
     private AtomicInteger totalRequest;
     private AtomicInteger successRequest;
     private String testMessage = "OK! This is a test!";
+    private List<Long> latencies = new ArrayList<>();
+
 
     public WSClient(String url, int iterations, AtomicInteger totalRequest, AtomicInteger successRequest) {
         Client client = ClientBuilder.newClient();
@@ -24,32 +28,39 @@ public class WSClient extends Thread {
     }
 
     public int postText(Object requestEntity) throws ClientErrorException {
-        totalRequest.getAndAdd(1);
-        int length = webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(requestEntity,
+        return webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(requestEntity,
                 javax.ws.rs.core.MediaType.TEXT_PLAIN),
                 Integer.class);
-
-        if (length == testMessage.length()) {
-            successRequest.getAndAdd(1);
-        }
-        return length;
     }
 
     public String getStatus() throws ClientErrorException {
-        totalRequest.getAndAdd(1);
-        String status = webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
-        if (status.equals("alive")){
-            successRequest.getAndAdd(1);
-        }
-        return status;
+        return webTarget.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
     }
 
     @Override
     public void run() {
         for (int i = 0; i < iterations; i++) {
-            this.getStatus();
-            this.postText(testMessage);
+            long start = System.currentTimeMillis();
+            String status = this.getStatus();
+            totalRequest.getAndAdd(1);
+            if (status.equals("alive")){
+                successRequest.getAndAdd(1);
+            }
+            long end = System.currentTimeMillis();
+            latencies.add(end - start);
+
+            start = System.currentTimeMillis();
+            int length = this.postText(testMessage);
+            totalRequest.getAndAdd(1);
+            if (length == testMessage.length()) {
+                successRequest.getAndAdd(1);
+            }
+            end = System.currentTimeMillis();
+            latencies.add(end - start);
         }
     }
 
+    public List<Long> getLatencies() {
+        return latencies;
+    }
 }
--- src/main/java/edu/neu/zhiyu/client/WSClientMain.java
+++ src/main/java/edu/neu/zhiyu/client/WSClientMain.java
@@ -1,7 +1,10 @@
 package edu.neu.zhiyu.client;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -21,6 +24,9 @@ public class WSClientMain {
         int ITERATIONS = 100;
         AtomicInteger totalRequest = new AtomicInteger(0);
         AtomicInteger successRequest = new AtomicInteger(0);
+
+        List<WSClient> clients = new ArrayList<>();
+        List<Long> latencies = new ArrayList<>();
 
         if (argv.length == 4) {
             MAX_THREAD = Integer.valueOf(argv[0]);
@@ -45,6 +51,7 @@ public class WSClientMain {
 
         for (int id = 0; id < MAX_THREAD; id++) {
             WSClient client = new WSClient(url, ITERATIONS, totalRequest, successRequest);
+            clients.add(client);
             ex.submit(client);
         }
 
@@ -59,6 +66,40 @@ public class WSClientMain {
         System.out.println("Total successful responses: " + successRequest);
         System.out.println();
         System.out.println("Current time: " + new Date().toString());
-        System.out.println("Total time taken: " + (end - start) + "ms");
+        System.out.println("Total time taken: " + (end - start) + "ms. \n");
+
+        System.out.println("Some stats: \n");
+
+        for (WSClient client : clients) {
+            for (Long lat : client.getLatencies()) {
+                latencies.add(lat);
+            }
+        }
+
+        Collections.sort(latencies);
+
+        int size = latencies.size();
+        System.out.println("Total latencies collected: " + size);
+
+        long leastLat = latencies.get(0);
+        long mostLat = latencies.get(size - 1);
+        long meanLat = getMeanLat(latencies);
+        long medianLat = latencies.get(latencies.size() / 2);
+        long lat95 = latencies.get((int) (latencies.size() * 0.95));
+        long lat99 = latencies.get((int) (latencies.size() * 0.99));
+        System.out.println("Least latency: " + leastLat + "ms");
+        System.out.println("Most latency: " + mostLat + "ms");
+        System.out.println("Mean latency: " + meanLat + "ms");
+        System.out.println("Median latency: " + medianLat + "ms");
+        System.out.println("95th percentile latency: " + lat95 + "ms");
+        System.out.println("99th percentile latency: " + lat99 + "ms");
+    }
+
+    private static long getMeanLat(List<Long> latencies) {
+        long sum = 0;
+        for(Long lat : latencies) {
+            sum += lat;
+        }
+        return sum / latencies.size();
     }
 }
